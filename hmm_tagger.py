b87e2f64e99c215abc3965c6971c97cb6c57cff1
emarkou <eleni.mark@yahoo.com>
2020-04-06 15:54:51 +0300

    feat: Initial project skeleton

---
+++ hmm_tagger.py
@@ -0,0 +1,19 @@
+# Implementation of an HMM tagger for part of speech tagging
+
+def pair_counts(sequences_A, sequences_B):
+    raise NotImplementedError
+
+
+def unigram_counts(sequences):
+    raise NotImplementedError
+
+def bigram_counts(sequences):
+    raise NotImplementedError
+
+def starting_counts(sequences):
+    raise NotImplementedError
+
+def ending_counts(sequences):
+    raise NotImplementedError
+
+
