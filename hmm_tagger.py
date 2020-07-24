3275b1b2a8b051e954f73f1a0e0f8e2fbf5b6d9a
emarkou <eleni.mark@yahoo.com>
2020-07-24 19:24:45 +0300

    feat: implement pair_counts, unigram_counts, bigram_counts, starting_counts & ending_counts

--- hmm_tagger.py
+++ hmm_tagger.py
@@ -1,19 +1,50 @@
 # Implementation of an HMM tagger for part of speech tagging
+from collections import Counter, defaultdict
 
 def pair_counts(sequences_A, sequences_B):
-    raise NotImplementedError
+    pair_count = defaultdict(dict)
+    sequences_A_flat = [element for sent in sequences_A for element in sent]
+    sequences_B_flat = [element for sent in sequences_B for element in sent]
+    for tag, word in zip(sequences_A_flat, sequences_B_flat):
+        try:
+            pair_count[tag][word]=pair_count[tag][word]+1
+        except KeyError:
+            pair_count[tag][word]=1
+    return pair_count
 
 
 def unigram_counts(sequences):
-    raise NotImplementedError
+    sequences_flat = [element for sent in sequences for element in sent]
+    return Counter(sequences_flat)
+
 
 def bigram_counts(sequences):
-    raise NotImplementedError
+    bigram_count = {}
+    for seq in sequences:
+        for idx in range(len(seq)):
+            if idx+1 < len(seq):
+                if (seq[idx], seq[idx+1]) in bigram_count:
+                    bigram_count[(seq[idx], seq[idx+1])]+=1
+                else:
+                    bigram_count[(seq[idx], seq[idx+1])]=1
+    return bigram_count
 
 def starting_counts(sequences):
-    raise NotImplementedError
+    tag_starts = {}
+    for seq in sequences:
+            if seq[0] in tag_starts:
+                tag_starts[seq[0]]+=1
+            else:
+                tag_starts[seq[0]]=1
+    return tag_starts
+
 
 def ending_counts(sequences):
-    raise NotImplementedError
-
+    tag_ends = {}
+    for seq in sequences:
+            if seq[-1] in tag_ends:
+                tag_ends[seq[-1]]+=1
+            else:
+                tag_ends[seq[-1]]=1
+    return tag_ends
 
